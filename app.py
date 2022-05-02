62a105b24ae46ad047b9714cae2fbc15965649dd
Collin <wildkard91@gmail.com>
2022-05-02 16:20:51 -0400

    initialized db

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primar_key=True)
+    content = db.COlumn(db.String(250), nullabe=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
 @app.route('/')
 
